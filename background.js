45ece40fbf47a7e0e2b2e476d5d7b1f26dfc4418
ChrisAchinga <achinga.chris@gmail.com>
2021-02-19 12:15:37 +0300

    show alert on any page load

--- background.js
+++ background.js
@@ -1,1 +1,22 @@
 alert('hi')
+
+const MyGenericClick = (info, tab) => {
+  console.log('Clicked on Page:', info, tab)
+}
+
+const MyImageClick = (info, tab) => {
+  console.log("Clicked an Image:", info, tab)
+}
+
+chrome.contextMenus.create({
+  "title": 'Share',
+  "contexts": ['page'],
+  "onclick": MyGenericClick,
+})
+
+chrome.contextMenus.create({
+    "title": 'Share Image',
+    "contexts": ['page'],
+    "onclick": MyImageClick,
+  })
+  
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("my extension")
+
+alert('Loaded')
